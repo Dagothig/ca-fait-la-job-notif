e8a004b4f76d27313ec362c683f70cc40f1f8b5f
dagothig <guillaumenoelmartel@gmail.com>
2022-10-02 17:08:00 -0400

    Weee

---
+++ ircanswer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ssh dagothig@165.227.34.49 tmux send-keys -l "$1"
+#&&
+#ssh dagothig@165.227.34.49 tmux send-keys Enter
--- ircnotif.sh
+++ ircnotif.sh
@@ -1,11 +1,20 @@
-last=$(ssh dagothig@165.227.34.49 \
-  tail -n 5 irclogs/liberachat/#diroum.log)
-title=$(echo "$last" | tail -n 1)
+#!/bin/bash
+
+last=$(
+  ssh dagothig@165.227.34.49 tail -n 10 "irclogs/liberachat/#diroum.log" \
+  | grep -o "<.*>" \
+  | tac)
+ircnotif=$(readlink -f "$0")
+dir=$(dirname $ircnotif)
+ircanswer="$dir/ircanswer.sh"
+
 termux-notification \
   -i ircnotif \
-  -t "$title" \
-  -c "$last" \
+  -t "head -n 1 $last" \
+  -c "tail -n 4 $last" \
   --alert-once \
   --ongoing \
   --button1 Répondre \
-  --button1-action "ssh dagothig@165.227.34.49 tmux send-keys -l '\$REPLY' && ssh dagothig@165.227.34.49 tmux send-keys Enter && sh /data/data/com.termux/files/home/ircnotif.sh || termux-toast Feck"
+  --button1-action "sh $ircanswer \"\$REPLY\" && sh $ircnotif"
+  --button2 Rafraîchir
+  --button2-action "sh $ircnotif"
