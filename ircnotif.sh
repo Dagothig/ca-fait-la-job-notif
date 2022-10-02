460cf2621935caa2e5b4a95171ac6e56fdd2d0ff
dagothig <guillaumenoelmartel@gmail.com>
2022-10-02 17:40:25 -0400

    Wiwi

--- ircnotif.sh
+++ ircnotif.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 last=$(
-  ssh dagothig@165.227.34.49 grep -o "<\.\*>" "irclogs/liberachat/#diroum.log" \
+  ssh dagothig@165.227.34.49 tail -n 20 cadeau-\#diroum.log \
+  | grep "<.*>" \
   | tail -n 5 \
   | tac)
 ircnotif=$(readlink -f "$0")
 dir=$(dirname $ircnotif)
 ircanswer="$dir/ircanswer.sh"
-title=$(echo $last | head -n 1)
-content=$(echo $last | tail -n 4)
+title=$(echo "$last" | head -n 1)
+content=$(echo "$last" | tail -n 4)
 
 termux-notification \
   -i ircnotif \
