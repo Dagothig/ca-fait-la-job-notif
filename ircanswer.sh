030fee9a67cadb719c239dffef6702b6c9d44f86
dagothig <guillaumenoelmartel@gmail.com>
2022-10-02 21:02:48 -0400

    Wiwi

--- ircanswer.sh
+++ ircanswer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "$2"
-keys=${2//[\']/"\"\'\""}
+keys=echo "$2" | sed "s:':\"'\":g"
 echo "$keys"
-#ssh $1 tmux send-keys -l \'"$keys"\'
+ssh $1 tmux send-keys -l \'"$keys"\'
 # && ssh $1 tmux send-keys Enter
